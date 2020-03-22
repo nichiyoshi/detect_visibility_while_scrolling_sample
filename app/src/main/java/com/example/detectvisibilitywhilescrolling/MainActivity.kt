2958ddbf80fdaa2172f85119be92b45b5a9b36d4
nichiyoshi <40135056+nichiyoshi@users.noreply.github.com>
2020-03-23 00:53:12 +0900

    first commit

---
+++ app/src/main/java/com/example/detectvisibilitywhilescrolling/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.detectvisibilitywhilescrolling
+
+import android.graphics.Rect
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.observe
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.distinctUntilChanged
+import com.example.detectvisibilitywhilescrolling.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val scrollBounds = Rect()
+
+        viewModel.visibilityChanged.observe(this) {
+            if(it) {
+                Toast.makeText(this, "Text Four SHOWN!!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Text Four HIDDEN!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.scrollView.apply {
+            setOnScrollChangeListener { _, _, _, _, _ ->
+                this.getHitRect(scrollBounds)
+                val isTextFourVisible = binding.textFour.getLocalVisibleRect(scrollBounds)
+                viewModel.toggleIsVisible(isTextFourVisible)
+            }
+        }
+    }
+}
+
+class MainViewModel: ViewModel() {
+
+    private val visibilityChangedMutable = MutableLiveData<Boolean>().apply { postValue(false) }
+    val visibilityChanged: LiveData<Boolean> get() = visibilityChangedMutable.distinctUntilChanged()
+
+    fun toggleIsVisible(isVisible: Boolean) {
+        if(visibilityChangedMutable.value != isVisible) {
+            visibilityChangedMutable.postValue(isVisible)
+        }
+    }
+
+}
